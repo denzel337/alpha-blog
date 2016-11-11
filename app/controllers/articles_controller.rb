0144385321bf2684360b18b1fba9c0c09f813be3
Denzel Lee <denzellee02@gmail.com>
2016-11-11 17:42:48 +0000

    extract rundacy into a method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     def index
         @articles = Article.all
     end    
@@ -6,7 +7,7 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def edit
-        @article = Article.find(params[:id])
+        
     end
     def create
         @article = Article.new(article_params)
@@ -19,7 +20,7 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Article was sucessfully updated"
             redirect_to article_path(@article)
@@ -28,17 +29,20 @@ class ArticlesController < ApplicationController
         end    
     end 
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         flash[:notice] = "Article been deleted"
         redirect_to articles_path
     end
     def show
-        @article = Article.find(params[:id])
+       
     end
     
     
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
     def article_params
         params.require(:article).permit(:title, :description)
     end
