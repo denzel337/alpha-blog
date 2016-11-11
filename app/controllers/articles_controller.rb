acb119209fb6ee385668be3e31de77345608c003
Denzel Lee <denzellee02@gmail.com>
2016-11-11 17:23:22 +0000

    add partials & destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,12 @@ class ArticlesController < ApplicationController
             render 'edit'
         end    
     end 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article been deleted"
+        redirect_to articles_path
+    end
     def show
         @article = Article.find(params[:id])
     end
