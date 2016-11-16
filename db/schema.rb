6173e5f570b9531e89a5423b9316e6a27e0ee219
Denzel Lee <denzellee02@gmail.com>
2016-11-16 06:29:31 +0000

    install bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20161103164432) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.text     "description"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
