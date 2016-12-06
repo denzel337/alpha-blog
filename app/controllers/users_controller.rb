907007a7e67dd6b1bd475eea2ae396f6a69ac78d
Denzel Lee <denzellee02@gmail.com>
2016-12-06 05:01:51 +0000

    Redirect to user home upon signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     if @user.save
         session[:user_id] = @user.id
         flash[:success] = "Welcome to the alpha blog #{@user.username}"
-        redirect_to articles_path
+        redirect_to user_path(@user)
     else
         render 'new'
     end
